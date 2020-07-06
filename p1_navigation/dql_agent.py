c1a71b29b6ea16acf12d54c1e485d08546cc4655
Ferenc Acs <dr.ferenc.acs@gmail.com>
2020-07-06 19:14:44 +0200

    feat: score plot & automatic filenames
    The learning score over all training runs is plotted now.
    Trained networks are saved automatically.

--- p1_navigation/dql_agent.py
+++ p1_navigation/dql_agent.py
@@ -19,6 +19,7 @@ UPDATE_EVERY = 4        # how often to update the network
 
 device = torch.device("cuda:0" if torch.cuda.is_available() else "cpu")
 
+
 class Agent():
     """Interacts with and learns from the environment."""
 
