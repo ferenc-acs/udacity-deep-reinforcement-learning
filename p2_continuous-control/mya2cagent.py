fix: policy estimation with logprob
Fixed: policy estimation now works

RuntimeError: Trying to backward through the graph a second time,
but the buffers have already been freed...

	modified:   p2_continuous-control/Continuous_Control.ipynb
	modified:   p2_continuous-control/mya2cagent.py
	modified:   p2_continuous-control/mya2cnet.py

p2_continuous-control/mya2cagent.py
@@ -6,6 +6,8 @@ import numpy as np
 from itertools import count
 import mya2cnet as mynet
 
+import pdb # Debug! Debug! Debug! Debug! Debug! Debug! Debug! Debug!
+
 
 class a2cagent():
     def __init__(self, numworkers, env, brain, max_steps = 10000, policy_loss_weight = 1.0, value_loss_weight = 0.6, entropy_loss_weight = 0.001):
@@ -49,7 +51,8 @@ class a2cagent():
         
         for step in count(start=1):
             
-            import pdb; pdb.set_trace() # Debug! Debug! Debug! Debug! Debug! Debug! Debug! Debug!
+            if step > 10: # Debug! Debug! Debug! Debug! Debug! Debug! Debug! Debug!
+                pdb.set_trace() # Debug! Debug! Debug! Debug! Debug! Debug! Debug! Debug!
             
         #for step in range(1): # Debug! Debug! Debug! Debug! Debug! Debug! Debug! Debug!       
             print(f'\rTraining epoch: {step} ', end = (lambda x: '#' if x%2 == 0 else '+')(step) )
@@ -110,7 +113,7 @@ class a2cagent():
         value_error = returns - values.detach().numpy().flatten() #Because Pytorch 0.4.0 %-O
         value_loss = np.mean( np.multiply( np.power(value_error, 2), 0.5 ) )
         
-        policy_loss = -1 * torch.mean( discount_gaes.detach() * logpas)
+        policy_loss = -1 * torch.mean( discount_gaes.detach() * torch.mean(logpas, 2)[-T:-1])
             
         #entropy_loss = -1 * np.mean(entropies.numpy()) #Not good! We need a Tensor!
         #entropy_loss = -1 * entropies.mean()
@@ -139,7 +142,7 @@ class a2cagent():
         #FA; BMSoT: OBSOLETE
         #for state in states:
         #actions, is_exploratories, logpasses, entropies, values = self.a2c_net.fullpass(states)
-        actions, values = self.a2c_net.fullpass(states)
+        actions, values, logpasses = self.a2c_net.fullpass(states)
         #actionsL.append(actions)
         #is_exploratoriesL.append(is_exploratories)
         #valuesL.append(values)
@@ -156,7 +159,7 @@ class a2cagent():
         
         # Thx2: https://stackoverflow.com/a/6383390/12171415
         #try:
-        #self.logpas.append(logpasses)
+        self.logpas.append(logpasses)
         #except AttributeError:
         #    self.logpas = torch.stack( torch.Tensor(logpas) )
 

p2_continuous-control/mya2cnet.py
@@ -13,6 +13,8 @@ HIDDEN_DIMS_DEFAULT = {
 
 # Thx2: https://livebook.manning.com/book/grokking-deep-reinforcement-learning/chapter-11/
 # Thx2: https://github.com/mimoralea/gdrl/blob/master/notebooks/chapter_11/chapter-11.ipynb
+# Thx2: https://discuss.pytorch.org/t/understanding-log-prob-for-normal-distribution-in-pytorch/73809
+
 class A2CNetwork(nn.Module):
     def __init__(self, input_dim, output_dim, max_grad_norm = 1, hidden_dims = HIDDEN_DIMS_DEFAULT):
         
@@ -87,15 +89,15 @@ class A2CNetwork(nn.Module):
     
     def fullpass(self, states):
         
-        import pdb; pdb.set_trace() # Debug! Debug! Debug! Debug! Debug! Debug! Debug! Debug!
+        #import pdb; pdb.set_trace() # Debug! Debug! Debug! Debug! Debug! Debug! Debug! Debug!
         
-        _, value = self.forward(states)
+        logits, value = self.forward(states)
         #dist = torch.distributions.Categorial( logits = logits )
         #dist = torch.distributions.categorical.Categorical( logits = logits ) #PyTorch 0.4.0
-        #dist = torch.distributions.normal.Normal( logits, torch.std(logits) )
+        dist = torch.distributions.normal.Normal( torch.mean(logits), torch.std(logits) )
         #action = dist.sample()
         action = self.select_action(states)
-        #logprob = dist.log_prob(action).unsqueeze(-1)
+        logprob = dist.log_prob(action).unsqueeze(-1)
         #logprob = 0.1 # Debug! Debug! Debug! Debug! Debug! Debug! Debug! Debug!
         #entropy = dist.entropy().unsqueeze(-1)
         #entropy = 0.1 # Debug! Debug! Debug! Debug! Debug! Debug! Debug! Debug!
@@ -103,7 +105,7 @@ class A2CNetwork(nn.Module):
         #action = F.hardtanh(logits) #FA: Limit to values between -1 and 1 
         #is_exploratory = action != np.argmax( logits.detach().numpy(), axis = int( len(state) != -1) )
         #is_exploratory = False # Debug! Debug! Debug! Debug! Debug! Debug! Debug! Debug!
-        return action, value #, logprob, entropy, is_exploratory
+        return action, value, logprob #, , entropy, is_exploratory
     
     def select_action(self, states):
         logits, _ = self.forward(states)